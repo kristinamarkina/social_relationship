7a70296151355485ec1b3f55f7cd06ff4326333b
kristinamarkina <kr.markina@gmail.com>
2023-11-29 20:55:04 -0500

    Adding remove user, remove relationship and clear graph to web page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+# app.py
+from flask import Flask, render_template, request, jsonify
 from graph import SocialGraph
 
 app = Flask(__name__)
@@ -6,7 +7,7 @@ social_graph = SocialGraph()
 
 @app.route("/")
 def index():
-    return render_template("index.html", graph=str(social_graph))
+    return render_template("index.html", graph=social_graph.to_json())
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
@@ -17,7 +18,18 @@ def add_user():
     except ValueError as e:
         message = str(e)
 
-    return render_template("index.html", graph=str(social_graph), message=message)
+    return render_template("index.html", graph=social_graph.to_json(), message=message)
+
+@app.route("/remove_user", methods=["POST"])
+def remove_user():
+    try:
+        user = request.form.get("user")
+        social_graph.remove_user(user)
+        message = f"User '{user}' removed successfully."
+    except ValueError as e:
+        message = str(e)
+
+    return render_template("index.html", graph=social_graph.to_json(), message=message)
 
 @app.route("/add_relationship", methods=["POST"])
 def add_relationship():
@@ -29,7 +41,29 @@ def add_relationship():
     except ValueError as e:
         message = str(e)
 
-    return render_template("index.html", graph=str(social_graph), message=message)
+    return render_template("index.html", graph=social_graph.to_json(), message=message)
+
+@app.route("/remove_relationship", methods=["POST"])
+def remove_relationship():
+    try:
+        user1 = request.form.get("user1")
+        user2 = request.form.get("user2")
+        social_graph.remove_relationship(user1, user2)
+        message = f"Relationship removed between '{user1}' and '{user2}' successfully."
+    except ValueError as e:
+        message = str(e)
+
+    return render_template("index.html", graph=social_graph.to_json(), message=message)
+
+@app.route("/clear", methods=["POST"])
+def clear():
+    social_graph.clear()
+    message = "Graph cleared successfully."
+    return render_template("index.html", graph=social_graph.to_json(), message=message)
+
+@app.route("/graph_json")
+def get_graph_json():
+    return jsonify(social_graph.to_json())
 
 if __name__ == "__main__":
     app.run(debug=True)
